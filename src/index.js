ba3816dcad1170d40b25bf47f25759bc9a8fa094
Fahad Ibnay Heylaal <fahad19@gmail.com>
2015-06-15 00:39:51 +0200

    finder logic.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import P from 'firenze/lib/Promise';
+import async from 'async';
 
 import Adapter from 'firenze/lib/Adapter';
 
@@ -70,27 +71,123 @@ export default class Memory extends Adapter {
   }
 
   read(q) {
-    let records = _.filter(this.data[q.table], (record) => {
-      return true; // apply conditions
+    return new P.resolve(this.findRecords(q));
+  }
+
+  findRecords(q) {
+    return new P((resolve, reject) => {
+      return async.waterfall([
+        (cb) => {
+          // all
+          let records this.data[q.table];
+          return cb(null, records);
+        },
+        (records, cb) => {
+          // conditions
+          if (!q.conditions) {
+            return cb(null, records);
+          }
+
+          let conditions = {};
+          _.each(q.conditions, (v, k) => {
+            if (k.indexOf('.') > -1) {
+              k = k.split('.')[1];
+              conditions[k] = v;
+            }
+          });
+
+          let filtered = _.filter(records, _.matches(conditions));
+          return cb(null, filtered);
+        },
+        (records, cb) => {
+          // sort
+          if (!q.order) {
+            return cb(null, records);
+          }
+
+          let order;
+          if (_.isObject(q.order)) {
+            order = [];
+            _.each(q.order, (v, k) => {
+              order.push({
+                [k]: v
+              })
+            });
+          } else {
+            order = q.order;
+          }
+
+          let sortFields = [];
+          let sortOrders = [];
+
+          order.forEach((o) => {
+            let k = _.keys(o)[0];
+            let v = _.values(o)[0];
+
+            if (k.indexOf('.') > -1) {
+              k = k.split('.')[1];
+            }
+
+            sortFields.push(k);
+            sortOrders.push(v.toLowerCase() === 'asc' ? true : false);
+          });
+
+          let sorted = _.sortByOrder(records, sortFields, sortOrders);
+          return cb(null, sorted);
+        },
+        (records, cb) => {
+          // limit
+          let page = q.page ? q.page : 1;
+          let limit = q.limit ? q.limit : null;
+
+          if (!limit) {
+            return cb(null, records);
+          }
+
+          let from = (page - 1) * limit;
+          let to = from + limit;
+          let limited = records.slice(from, to);
+          return cb(null, limited);
+        }
+      ], (err, result) => {
+        if (err) {
+          return reject(err);
+        }
+
+        return resolve(result);
+      });
     });
-    return new P.resolve(records);
   }
 
   update(q, obj) {
-    let k = _.findIndex(this.data[q.table], (r) => {
-      return true; // apply conditions
-    });
+    return new P((resolve, reject) => {
+      let records = this.findRecords(q);
+      if (records.length === 0) {
+        return reject(false);
+      }
 
-    this.data[q.table][k] = _.merge(this.data[q.table][k], obj);
-    return new P.resolve(this.data[q.table][k]);
+      let id = records[0][q.primaryKey];
+      let k = _.findIndex(this.data[q.table], (r) => {
+        return r[q.primaryKey] === id;
+      });
+      this.data[q.table][k] = _.merge(this.data[q.table][k], obj);
+      return resolve(this.data[q.table][k]);
+    });
   }
 
   delete(q) {
-    let k = _.findIndex(this.data[q.table], (r) => {
-      return true; // apply conditions
-    });
+    return new P((resolve, reject) => {
+      let records = this.findRecords(q);
+      if (records.length === 0) {
+        return reject(false);
+      }
 
-    this.data[q.table].splice(k, 1);
-    return new P.resolve(true);
+      let id = records[0][q.primaryKey];
+      let k = _.findIndex(this.data[q.table], (r) => {
+        return r[q.primaryKey] === id;
+      });
+      this.data[q.table].splice(k, 1);
+      return resolve(true);
+    });
   }
 }
