a8b891860795c45d30ea9a22aa00171bd7a5b1ba
Fahad Ibnay Heylaal <fahad19@gmail.com>
2015-06-14 21:08:52 +0200

    base for MemoryAdapter operations.

--- src/index.js
+++ src/index.js
@@ -30,13 +30,67 @@ export default class Memory extends Adapter {
   }
 
   getConnection() {
-    return this.knex;
+    return this.data;
   }
 
   closeConnection(cb = null) {
     if (!cb) {
       cb = function () { };
     }
-    return this.getConnection().destroy(cb);
+    return cb();
+  }
+
+  dropTable(model) {
+    this.data = _.omit(this.data, model.collection().table);
+    return new P.resolve(true);
+  }
+
+  createTable(model) {
+    this.data[model.collection().table] = [];
+    return new P.resolve(true);
+  }
+
+  populateTable(model, rows) {
+    this.data[model.collection().table] = rows;
+    return new P.resolve(true);
+  }
+
+  query(collection, options = {}) {
+    return _.merge(options, {
+      table: collection.table,
+      alias: collection.model().alias,
+      primaryKey: collection.model().primaryKey
+    });
+  }
+
+  create(q, obj) {
+    obj[q.primaryKey] = _.uniqueId();
+    this.data[q.table].push(obj);
+    return new P.resolve(obj);
+  }
+
+  read(q) {
+    let records = _.filter(this.data[q.table], (record) => {
+      return true; // apply conditions
+    });
+    return new P.resolve(records);
+  }
+
+  update(q, obj) {
+    let k = _.findIndex(this.data[q.table], (r) => {
+      return true; // apply conditions
+    });
+
+    this.data[q.table][k] = _.merge(this.data[q.table][k], obj);
+    return new P.resolve(this.data[q.table][k]);
+  }
+
+  delete(q) {
+    let k = _.findIndex(this.data[q.table], (r) => {
+      return true; // apply conditions
+    });
+
+    this.data[q.table].splice(k, 1);
+    return new P.resolve(true);
   }
 }
